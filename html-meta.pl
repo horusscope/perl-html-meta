381fd947d2d96f59fe90c009aa52d863c66f3abd
horusscope <horusscope@gmail.com>
2019-05-02 13:38:52 -0700

    added basic meta head parsing
    collecting the basic meta elements
    printing the base demo

--- html-meta.pl
+++ html-meta.pl
@@ -23,17 +23,45 @@ sub load {
 }
 
 my $result = (load "cache/$id", "$req");
-my @props = grep {$_} map {
-	$a = $_->attr('property');
-	if( ! $a ) { $a = $_->attr('name') }
-	if( ! $a ) {0}
+sub props {
+	grep {$_} map {
+		$a = $_->attr('property');
+		if( ! $a ) {$a = $_->attr('name')}
+		if( ! $a ) {0}
 
-	$b = $_->attr('content');
-	if( ! $b ) { $b = $_->attr('value') }
-	if( ! $b ) {0}
-
-	[ $a, $b ];
-} $result->find('head meta')->each;
-
-print Dumper(@props);
+		$b = $_->attr('content');
+		if( ! $b ) {$b = $_->attr('value')}
+		if( ! $b ) {0}
+		
+		[$a, $b];
+	} shift->each;
+}
+sub card {
+	my $dom = shift;
+	my ($meta, $link, $script) =	( $dom->find('head meta')
+					, $dom->find('head link')
+					, $dom->find('head script') );
+	
+	my @propMeta = props($meta);
+	@title = grep { $_->[0] =~ /^(title|og:title|twitter:title)$/ } @propMeta;
+	@desc = grep { $_->[0] =~ /^(description|og:description|twitter:description)$/ } @propMeta;
+	@image = grep { $_->[0] =~ /^(image|og:image|twitter:image|twitter:image:src)$/ } @propMeta;
+	@alt = grep { $_->[0] =~ /^(alt|og:alt|twitter:image:alt)$/ } @propMeta;
+	@siteName = grep { $_->[0] =~ /^(site_name|og:site_name|twitter:site_name)$/ } @propMeta;
+	@site = grep { $_->[0] =~ /^(site|og:site|twitter:site)$/ } @propMeta;
+	@type = grep { $_->[0] =~ /^(type|og:type|twitter:type)$/ } @propMeta;
+	@author = grep { $_->[0] =~ /^(creator|author|og:creator|twitter:creator|og:author|twitter:author)$/ } @propMeta;
+	( 'title' => @title? $title[0][1]: ''
+	, 'description' => @desc? $desc[0][1]: ''
+	, 'image' => @image? $image[0][1]: ''
+	, 'alt' => @alt? $alt[0][1]: ''
+	, 'site_name' => @siteName? $siteName[0][1]: ''
+	, 'site' => @site? $site[0][1]: ''
+	, 'type' => @type? $type[0][1]: ''
+	, 'author' => @author? $author[0][1]: ''
+	);
+}
+%meta = card $result;
+$json = JSON::XS->new->canonical->pretty->encode(\%meta);
+print $json;
 
